c17f1a049125434612ad4a85e0968a973ccc9a95
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2024-03-14 19:32:27 -0300

    agrego codigos a index1.js

--- laplacita/index1.js
+++ laplacita/index1.js
@@ -57,3 +57,21 @@ console.log(operacion(3200, 5900, 2420, 3200, 7600, 690, sumar)); // Resultado: 
 console.log(operacion(3200, 5900, 2420, 3200, 7600, 690, restar)); // Resultado: 2
 console.log(operacion(3200, 5900, 2420, 3200, 7600, 690, multiplicar)); // Resultado: 15
 console.log(operacion(3200, 5900, 2420, 3200, 7600, 690, dividir)); // Resultado: 3
+
+//setTimeout
+console.log("1. Inicio de la carga de datos de los productos (setTimeout)");
+
+function cargarDatosDesdeServidor(callback) {
+  setTimeout(function() {
+    const datos = "Datos de los productos cargados desde el servidor";
+    callback(datos);
+  }, 2000);
+}
+
+function procesarDatos(datos) {
+  console.log("4. Procesando datos:", datos);
+}
+
+console.log("2. Solicitando datos al servidor");
+cargarDatosDesdeServidor(procesarDatos);
+console.log("3. Fin de la carga de datos (setTimeout)");

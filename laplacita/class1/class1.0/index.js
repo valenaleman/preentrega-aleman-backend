179c2c01605cffdf6416cfed6f1edb7ea669e0d9
Valentina Lujan Aleman <valentinaaleman07@gmial.com>
2024-04-03 18:23:41 -0300

    agrego cambios de index.js de la carpeta class1.0

--- laplacita/class1/class1.0/index.js
+++ laplacita/class1/class1.0/index.js
@@ -1,4 +1,6 @@
 const _ = require("lodash");
+const fs = require('fs');
+const crypto = require('crypto');
 
 function generateRandomPrice(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -19,4 +21,44 @@ console.log('Resultados:');
 for (let number in countByNumber) {
 
     console.log(`Número ${number}: ${countByNumber[number]} veces.`);
+}
+
+class UserManager {
+    constructor(){
+        this.filename = "Usuarios.json";
+    }
+
+    createUser(user){
+        const hashedPassword = this.hashPassword(user.password);
+        const newUser = {
+            nombre: user.name,
+            apellido: user.apellido,
+            username: user.username,
+            password: user.password,
+        }
+
+        const existingUsers = this.readUsersFromFile();
+
+        existingUsers.push(newUser);
+
+        this.writeUserToFile(existingUsers);
+    }
+
+    hashPassword(password){
+        const hash = crypto.createHash('Valen03').update(password).digest('hex');
+        return hash;
+    }
+
+    readUserFromFile(){
+        try {
+            const data = fs.readFileSync(this.filename, 'utf-8');
+            return data;
+        }catch(error){
+            return[];
+        }
+    }
+
+    writeUserToFile(users){
+        fs.writeFileSync(this.filename, JSON.stringify(users, null, 2));
+    }
 }
